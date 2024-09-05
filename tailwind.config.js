9b9d6a4464c365fc6de9421722a3ac0c3265862f
Dirb <clarissa.aurelia1001@gmail.com>
2024-09-05 23:44:32 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'cream': '#F0F1E7',
+        'yellow': '#E8D583',
+        'red': '#DD3822',
+        'blue': '#4398C1',
+        'green': '#68C447'
+      }
+    },
+  },
+  plugins: [],
+}
