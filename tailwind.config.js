9c4a834b83872b8a0ac515bf078c12852e13f9e6
Dirb <clarissa.aurelia1001@gmail.com>
2024-09-06 07:40:46 +0700

    media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'cloud' : '#8c8c8c',
         'cream': '#F0F1E7',
         'yellow': '#E8D583',
         'red': '#DD3822',
         'blue': '#4398C1',
-        'green': '#68C447'
+        'green': '#68C447',
+        'cloud' : '#8c8c8c'
       }
     },
   },
